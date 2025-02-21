90dcaf58ef787be3133f5faec789a35d5a41e668
bragazx9 <bragabernardo454@gmail.com>
2025-02-21 00:44:14 -0300

    Segundo exercicio

---
+++ atividade2Scene/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module ativ2.atividade2scene {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens ativ2.atividade2scene to javafx.fxml;
+    exports ativ2.atividade2scene;
+}
