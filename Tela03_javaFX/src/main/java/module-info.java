484b3e611746b930269ea49cdb6f646c722521da
bragazx9 <bragabernardo454@gmail.com>
2025-02-24 21:02:22 -0300

    Add files via upload

---
+++ Tela03_javaFX/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module tela03.tela03_javafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens tela03.tela03_javafx to javafx.fxml;
+    exports tela03.tela03_javafx;
+}
